cac3de40ddfc0e570af4702d9cab26d518a5ade5
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-27 10:57:17 +0200

    Ejercicio 7 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,32 +1,47 @@
 package com.campusdual.classroom;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exercise07 {
 
     public static void main(String[] args) {
-
+        positionInAList(5);  // Cambia este número para probar diferentes entradas
+        sumFirstNaturalNumbers(5);  // Cambia este número para probar diferentes entradas
+        showFirstNaturalNumbers(5);  // Cambia este número para probar diferentes entradas
     }
 
-    //TODO ↓
-    // ESTE MÉTODO NO ES NECESARIO HACERLO POR PARTE DE LOS ALUMNOS
-    // Crear una List<Integer> con los números del 10 al 1. Luego buscar el elemento
-    // que se le pasa por parámetro.
-    // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
-    // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
+
     public static void positionInAList(int num) {
+        List<Integer> numbers = new ArrayList<>();
 
+        for (int i = 0; i <= 10; i++) {
+            numbers.add(i);
+        }
+
+        int index = numbers.indexOf(num);
+
+        if (index != -1) {
+            System.out.println("El elemento " + num + " se encuentra en la posición: " + index);
+        } else {
+            System.out.println("El elemento " + num + " no se encuentra en la lista.");
+        }
     }
 
-    //TODO ↓
-    // Que imprima como mensaje la suma de los primeros N números positivos
+
     public static void sumFirstNaturalNumbers(int num) {
-
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println("La suma de los primeros " + num + " números positivos es: " + sum);
     }
 
-    //TODO ↓
-    // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
-
+        System.out.print("Los primeros " + num + " números positivos son: ");
+        for (int i = 1; i <= num; i++) {
+            System.out.print(i + (i < num ? ", " : "."));
+        }
+        System.out.println();  // Nueva línea al final
     }
-
 }
